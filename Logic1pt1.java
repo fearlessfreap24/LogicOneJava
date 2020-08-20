95366f0426bd3872c29d61bca04b0e4275ca7a14
fearlessfreap24 <dylanaperez@yahoo.com>
2020-08-20 13:39:03 -0500

    completed squirrelPlay, caughtSpeeding, sortaSum, and alarmClock

--- Logic1pt1.java
+++ Logic1pt1.java
@@ -1,4 +1,4 @@
-package bat.coding.one.logic;
+//package bat.coding.one.logic;
 
 //Basic boolean logic puzzles -- if else && || !.
 
@@ -19,13 +19,40 @@ public class Logic1pt1 {
 //                    cigarParty(nums1[i], bools1[i]));
 //        }
 
-        int[][] nums2 = { {5, 10}, {5, 2}, {5, 5} };
-        for ( int[] nums : nums2 ){
-            System.out.printf("you = %d, date = %d : %d\n", nums[0], nums[1], dateFashion(nums[0], nums[1]));
-        }
+//        int[][] nums2 = { {5, 10}, {5, 2}, {5, 5} };
+//        for ( int[] nums : nums2 ){
+//            System.out.printf("you = %d, date = %d : %d\n", nums[0], nums[1], dateFashion(nums[0], nums[1]));
+//        }
+    	
+//    	int[] nums3 = { 70, 95, 95 };
+//    	boolean[] bools3 = { false, false, true };
+//    	for ( int i = 0; i < nums3.length; i++ ) {
+//    		System.out.printf("temp = %d, isSummer = %b : %b\n", nums3[i], bools3[i],
+//    				squirrelPlay(nums3[i], bools3[i]));
+//    	}
+    	
+//    	int[] nums4 = { 60, 65, 65 };
+//    	boolean[] bools4 = { false, false, true };
+//    	for ( int i = 0; i < nums4.length; i++ ) {
+//    		System.out.printf("speed = %d, isBirthday = %b : %d\n", nums4[i], bools4[i],
+//    				caughtSpeeding(nums4[i], bools4[i]));
+//    	}
+    	
+//    	int[][] nums5 = { {3, 4}, {9, 4}, {10, 11} };
+//    	for ( int[] nums : nums5 ) {
+//    		System.out.printf("a = %d, b = %d : %d\n", nums[0], nums[1],
+//    				sortaSum(nums[0], nums[1]));
+//    	}
+    	
+    	int[] nums6 = { 1, 5, 0 };
+    	boolean[] bools6 = { false, false, false };
+    	for ( int i = 0; i < nums6.length; i++ ) {
+    		System.out.printf("day = %d, vacation = %b : %s\n", nums6[i], bools6[i],
+    				alarmClock(nums6[i], bools6[i]));
+    	}
     }
 
-    private boolean cigarParty(int cigars, boolean isWeekend) {
+	private boolean cigarParty(int cigars, boolean isWeekend) {
 
 //        When squirrels get together for a party, they like to
 //        have cigars. A squirrel party is successful when the
@@ -65,4 +92,77 @@ public class Logic1pt1 {
         }
         return 2;
     }
+
+    private boolean squirrelPlay(int temp, boolean isSummer) {
+
+//    	The squirrels in Palo Alto spend most of the day playing. 
+//    	In particular, they play if the temperature is between 60 
+//    	and 90 (inclusive). Unless it is summer, then the upper 
+//    	limit is 100 instead of 90. Given an int temperature and a 
+//    	boolean isSummer, return true if the squirrels play and false 
+//    	otherwise.
+//
+//		squirrelPlay(70, false) → true
+//		squirrelPlay(95, false) → false
+//		squirrelPlay(95, true) → true
+		
+    	return (temp >= 60 && temp <= 90 && !isSummer) 
+    			|| ( temp >= 60 && temp <= 100 && isSummer ); 
+	}
+
+	private int caughtSpeeding(int speed, boolean isBirthday) {
+
+//		You are driving a little too fast, and a police officer stops 
+//		you. Write code to compute the result, encoded as an int value: 
+//		0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, 
+//		the result is 0. If speed is between 61 and 80 inclusive, the 
+//		result is 1. If speed is 81 or more, the result is 2. Unless it is 
+//		your birthday -- on that day, your speed can be 5 higher in all cases.
+//
+//		caughtSpeeding(60, false) → 0
+//		caughtSpeeding(65, false) → 1
+//		caughtSpeeding(65, true) → 0
+		int ticket = 0;
+		if ( isBirthday ) speed -= 5;
+		if ( speed > 60 && speed <= 80 ) ticket = 1;
+		if ( speed > 80 ) ticket = 2;
+		return ticket;
+		
+	}
+
+	private int sortaSum(int a, int b) {
+
+//		Given 2 ints, a and b, return their sum. However, sums in 
+//		the range 10..19 inclusive, are forbidden, so in that case 
+//		just return 20.
+//
+//		sortaSum(3, 4) → 7
+//		sortaSum(9, 4) → 20
+//		sortaSum(10, 11) → 21
+		
+		int sortasum = a + b;
+		if ( sortasum >= 10 && sortasum <= 19 ) return 20;
+		return sortasum;
+	}
+
+	private String alarmClock(int day, boolean vacation) {
+
+//		Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...
+//		6=Sat, and a boolean indicating if we are on vacation, return 
+//		a string of the form "7:00" indicating when the alarm clock 
+//		should ring. Weekdays, the alarm should be "7:00" and on the 
+//		weekend it should be "10:00". Unless we are on vacation -- then 
+//		on weekdays it should be "10:00" and weekends it should be "off".
+//
+//
+//		alarmClock(1, false) → "7:00"
+//		alarmClock(5, false) → "7:00"
+//		alarmClock(0, false) → "10:00"
+		if ( day == 0 || day == 6 ) {
+			if ( vacation ) return "off";
+			return "10:00";
+		}
+		if ( vacation ) return "10:00";
+		return "7:00";
+	}
 }
